4ab8dda1dfb29ac77d57725ccb8d62c17c9c7c6c
banan1442 <banan1442@outlook.com>
2025-06-04 13:52:40 +0200

    1

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/unnt', function () {
-    return view('unnt');
-});
-
-Route::get('/home', function () {
     return view('home');
 });
